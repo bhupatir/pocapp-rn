6b2d599f466676309adb215eb520be6cba6f96c2
mac-os <bhupati@srivatech.com>
2016-09-29 03:44:18 -0700

    Added Slider control example

---
+++ components/slider/SliderExample.js
@@ -0,0 +1,54 @@
+var React = require('react-native');
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  SliderANDROID,
+} = React;
+
+class SliderExample extends React.Component{
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      age: 0,
+    }
+
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>
+          Age: {this.state.age}
+        </Text>
+        <SliderIOS
+          style={styles.slider}
+          minimumValue={0}
+          maximumValue={100}
+          step={1}
+          onValueChange={(age) => this.setState({age: age})} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'green',
+  },
+  text: {
+    color: 'white',
+    fontSize: 24,
+  },
+  slider: {
+    width: 300,
+  }
+});
+
+AppRegistry.registerComponent('SliderExample', () => SliderExample);
